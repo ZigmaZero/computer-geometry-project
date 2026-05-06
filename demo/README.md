900f4991e25d77a01cb2be6317ab130660502e95
ZigmaZero <wachirawichkanil@gmail.com>
2026-05-06 19:14:24 +0700

    stuff

--- demo/README.md
+++ demo/README.md
@@ -1,3 +1,5 @@
 # Demo
 
 You can demo the project interactively by using the Jupyter Notebook.
+
+For online interactivity, visit [the Google Colab link](https://colab.research.google.com/drive/1jdRwYRcDJDOGzIQ5vay7hfkIbgRoMvRO).
--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from data_structures.definitions import *
 from algorithms.proposed import proposed_algorithm
+from testing.benchmark.test_bench import TestBench
 from testing.inputs.input_generation import build_random_input
 from visualization.plot_utils import plot_faction_control
 
+# test_bench = TestBench(100, 100, 10, 3, 3, 2, 2)
+# test_bench.bench()
+
 # interactive constraints for generation~
 
-region_x = 10
-region_y = 10
-obstacle_count = 4
+region_x = 100
+region_y = 100
+obstacle_count = 20
 obstacle_vertex_min = 3
-obstacle_vertex_max = 3
-faction_count = 2
-agent_count = 2
+obstacle_vertex_max = 5
+faction_count = 4
+agent_count = 10
 
 interactiveTestInput = build_random_input(region_x, region_y, obstacle_count, obstacle_vertex_min, obstacle_vertex_max, faction_count, agent_count)
 proposed_algorithm(interactiveTestInput.region, interactiveTestInput.obstacles, interactiveTestInput.agentlist, interactiveTestInput.factions)
